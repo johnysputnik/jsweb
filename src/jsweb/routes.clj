4f2f808eff627040e9a6eba5caf0182207710bcb
John Cumming <john@jsolutions.co.uk>
2015-02-04 09:16:36 +0000

    JC - Added responsive layout

--- src/jsweb/routes.clj
+++ src/jsweb/routes.clj
@@ -9,5 +9,8 @@
 
 (defroutes app
            (GET "/" [] (templates/home {:title "jSolutions Ltd"}))
+           (GET "/articles" [] (templates/home {:title "latest articles"}))
+           (GET "/about" [] (templates/home {:title "about jSolutions"}))
+           (GET "/contact" [] (templates/home {:title "contact jSolutions"}))
            (route/resources "/")
            (route/not-found "not found"))
